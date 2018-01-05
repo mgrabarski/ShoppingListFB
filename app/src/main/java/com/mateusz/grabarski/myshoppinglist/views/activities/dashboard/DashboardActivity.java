47e80fdd96b662b49d0e0155dacf1ca9010d132b
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2018-01-05 21:01:31 +0100

    Added simple nevigation drawer

--- app/src/main/java/com/mateusz/grabarski/myshoppinglist/views/activities/dashboard/DashboardActivity.java
+++ app/src/main/java/com/mateusz/grabarski/myshoppinglist/views/activities/dashboard/DashboardActivity.java
@@ -1,15 +1,57 @@
 package com.mateusz.grabarski.myshoppinglist.views.activities.dashboard;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.mateusz.grabarski.myshoppinglist.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class DashboardActivity extends AppCompatActivity {
+
+    @BindView(R.id.activity_dashboard_toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.activity_dashboard_navigation)
+    NavigationView navigationView;
+
+    @BindView(R.id.activity_dashboard_drawer_layout)
+    DrawerLayout drawerLayout;
+
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+
+        drawerToggle = setupDrawerToggle();
+        drawerToggle.syncState();
     }
+
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        return new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
