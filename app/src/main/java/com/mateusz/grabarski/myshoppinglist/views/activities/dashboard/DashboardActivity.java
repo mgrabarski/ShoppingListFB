c7e2db155f0011e9c07a566f82ee6c84e544a426
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2018-01-09 12:53:11 +0100

    added dialog to get shooping list name

--- app/src/main/java/com/mateusz/grabarski/myshoppinglist/views/activities/dashboard/DashboardActivity.java
+++ app/src/main/java/com/mateusz/grabarski/myshoppinglist/views/activities/dashboard/DashboardActivity.java
@@ -10,15 +10,19 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 
 import com.mateusz.grabarski.myshoppinglist.R;
+import com.mateusz.grabarski.myshoppinglist.views.activities.dashboard.dialogs.GetShoppingListDialog;
 import com.mateusz.grabarski.myshoppinglist.views.activities.dashboard.fragments.ShoppingListFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class DashboardActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity implements
+        ShoppingListFragment.ShoppingListFragmentListener,
+        GetShoppingListDialog.GetShoppingListDialogInterface {
 
     @BindView(R.id.activity_dashboard_toolbar)
     Toolbar toolbar;
@@ -87,4 +91,14 @@ public class DashboardActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onAddNewShoppingListClick() {
+        GetShoppingListDialog dialog = GetShoppingListDialog.newInstance();
+        dialog.show(getSupportFragmentManager(), null);
+    }
+
+    @Override
+    public void onShoppingListNameTyped(String shoppingListName) {
+        Log.d(DashboardActivity.class.getSimpleName(), "onShoppingListNameTyped: " + shoppingListName);
+    }
 }
--- app/src/main/java/com/mateusz/grabarski/myshoppinglist/views/activities/dashboard/fragments/ShoppingListFragment.java
+++ app/src/main/java/com/mateusz/grabarski/myshoppinglist/views/activities/dashboard/fragments/ShoppingListFragment.java
@@ -1,5 +1,6 @@
 package com.mateusz.grabarski.myshoppinglist.views.activities.dashboard.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,11 +10,16 @@ import android.view.ViewGroup;
 
 import com.mateusz.grabarski.myshoppinglist.R;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 /**
  * Created by Mateusz Grabarski on 09.01.2018.
  */
 
 public class ShoppingListFragment extends Fragment {
+
+    private ShoppingListFragmentListener mListener;
 
     public static ShoppingListFragment newInstance() {
 
@@ -24,11 +30,31 @@ public class ShoppingListFragment extends Fragment {
         return fragment;
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        if (context instanceof ShoppingListFragmentListener)
+            mListener = (ShoppingListFragmentListener) context;
+        else
+            throw new RuntimeException("Activity must implement ShoppingListFragmentListener");
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_shopping_list, container, false);
+        ButterKnife.bind(this, view);
 
         return view;
+    }
+
+    @OnClick(R.id.fragment_shopping_list_fab)
+    public void onAddShoppingListClick() {
+        mListener.onAddNewShoppingListClick();
+    }
+
+    public interface ShoppingListFragmentListener {
+        void onAddNewShoppingListClick();
     }
 }
