07ce02b013f138261e80eefc105e6df4ff8bdabe
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2018-01-12 14:02:49 +0100

    Added edit profile layouts

--- app/src/main/java/com/mateusz/grabarski/myshoppinglist/views/activities/profile/EditProfileActivity.java
+++ app/src/main/java/com/mateusz/grabarski/myshoppinglist/views/activities/profile/EditProfileActivity.java
@@ -1,15 +1,63 @@
 package com.mateusz.grabarski.myshoppinglist.views.activities.profile;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.mateusz.grabarski.myshoppinglist.R;
+import com.mateusz.grabarski.myshoppinglist.views.activities.profile.fragments.MainEditProfileFragment;
 
-public class EditProfileActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class EditProfileActivity extends AppCompatActivity implements
+        MainEditProfileFragment.MainEditProfileFragmentListener {
+
+    @BindView(R.id.activity_edit_profile_toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        ButterKnife.bind(this);
+
+        if (getSupportFragmentManager().getBackStackEntryCount() == 0)
+            replaceAndAddToBackStackFragment(MainEditProfileFragment.newInstance());
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getSupportFragmentManager().getBackStackEntryCount() == 1) {
+            finish();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void replaceAndAddToBackStackFragment(Fragment fragment) {
+        String tag = fragment.getClass().getName();
+
+        FragmentManager manager = getSupportFragmentManager();
+        boolean fragmentPopped = manager.popBackStackImmediate(tag, 0);
+
+        if (!fragmentPopped && manager.findFragmentByTag(tag) == null) {
+            FragmentTransaction ft = manager.beginTransaction();
+            ft.replace(R.id.activity_edit_profile_fl, fragment, tag);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.addToBackStack(tag);
+            ft.commit();
+        }
+    }
+
+    @Override
+    public void onChangeAvatarClick() {
+
     }
 }
---
+++ app/src/main/java/com/mateusz/grabarski/myshoppinglist/views/activities/profile/fragments/MainEditProfileFragment.java
@@ -0,0 +1,84 @@
+package com.mateusz.grabarski.myshoppinglist.views.activities.profile.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mateusz.grabarski.myshoppinglist.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
+/**
+ * Created by MGrabarski on 12.01.2018.
+ */
+
+public class MainEditProfileFragment extends Fragment {
+
+    @BindView(R.id.fragment_main_edit_profile_name_et)
+    TextInputEditText nameEt;
+
+    @BindView(R.id.fragment_main_edit_profile_name_til)
+    TextInputLayout nameTil;
+
+    @BindView(R.id.fragment_main_edit_profile_email_et)
+    TextInputEditText emailEt;
+
+    @BindView(R.id.fragment_main_edit_profile_email_til)
+    TextInputLayout emailTil;
+
+    Unbinder unbinder;
+
+    private MainEditProfileFragmentListener mListener;
+
+    public static MainEditProfileFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        MainEditProfileFragment fragment = new MainEditProfileFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        if (context instanceof MainEditProfileFragmentListener)
+            mListener = (MainEditProfileFragmentListener) context;
+        else
+            throw new RuntimeException("Activity must implement MainEditProfileFragmentListener");
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main_edit_profile, container, false);
+
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @OnClick(R.id.fragment_main_edit_profile_avatar_iv)
+    public void onAvatarClick() {
+        mListener.onChangeAvatarClick();
+    }
+
+    public interface MainEditProfileFragmentListener {
+        void onChangeAvatarClick();
+    }
+}
