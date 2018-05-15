41b664cad8e82965e848d2ae351808c96be15382
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2018-05-15 14:18:10 +0200

    Added friend fragments

--- app/src/main/java/com/mateusz/grabarski/myshoppinglist/views/activities/dashboard/fragments/FriendsFragment.java
+++ app/src/main/java/com/mateusz/grabarski/myshoppinglist/views/activities/dashboard/fragments/FriendsFragment.java
@@ -2,18 +2,39 @@ package com.mateusz.grabarski.myshoppinglist.views.activities.dashboard.fragment
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.mateusz.grabarski.myshoppinglist.R;
+import com.mateusz.grabarski.myshoppinglist.views.activities.dashboard.adapters.FriendsViewPagerAdapter;
+import com.mateusz.grabarski.myshoppinglist.views.activities.dashboard.fragments.friends.FriendsListFragment;
+import com.mateusz.grabarski.myshoppinglist.views.activities.dashboard.fragments.friends.RequestFriendListFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by Mateusz Grabarski on 09.01.2018.
  */
 
 public class FriendsFragment extends Fragment {
+
+    @BindView(R.id.fragment_friends_list_tab)
+    TabLayout tabs;
+
+    @BindView(R.id.fragment_friends_list_pager)
+    ViewPager pager;
+
+    @BindView(R.id.fragment_friends_list_ll)
+    LinearLayout rootLl;
+
+    Unbinder unbinder;
 
     public static FriendsFragment newInstance() {
 
@@ -29,6 +50,23 @@ public class FriendsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_friends_list, container, false);
 
+        unbinder = ButterKnife.bind(this, view);
+
+        FriendsViewPagerAdapter adapter = new FriendsViewPagerAdapter(getActivity().getSupportFragmentManager());
+
+        adapter.addFragment(FriendsListFragment.newInstance(), getString(R.string.friend));
+        adapter.addFragment(RequestFriendListFragment.newInstance(), getString(R.string.request));
+
+        pager.setAdapter(adapter);
+
+        tabs.setupWithViewPager(pager);
+
         return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
